0de4d3829c5fee19e150e890450023915db57783
ishaan-bose <ishaan.varshney.bose@gmail.com>
2020-09-11 14:00:37 +0530

    why do i have to write these messages

--- others/src/Collider.cpp
+++ others/src/Collider.cpp
@@ -1,7 +1,4 @@
 #include "Collider.hpp"
 
-Collider::Collider(float zero_x,float zero_y,float p_w,float p_h)
-    :pos00(zero_x,zero_y), downleft(pos00.x, pos00.y + p_h), 
-    downright(pos00.x + p_w, pos00.y + p_h), upleft(pos00.x, pos00.y),
-    upright(pos00.x + p_w, pos00.y)
+Collider::BoxCollider(float zero_x,float zero_y,float p_w,float p_h)
 {}
