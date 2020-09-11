93ee7a0ec38d1bd66ebc498e380209e32e3c5b6f
ishaan-bose <ishaan.varshney.bose@gmail.com>
2020-09-11 13:16:14 +0530

    added an equality check

--- others/include/Position.hpp
+++ others/include/Position.hpp
@@ -6,4 +6,11 @@ struct Position
     Position(float p_x, float p_y)
         :x(p_x), y(p_y) {}
     Position() = default;
+    
+    Position operator==(Position& other) const;
 };
+
+Position::Position operator==(Position& other) const
+{
+    return((x==other.x) && (y==other.y));
+}
