0b9b25b05057fbfe1c88dc0205b7726297c74908
ishaan-bose <ishaan.varshney.bose@gmail.com>
2020-09-11 15:32:56 +0530

    added a function

--- others/include/Position.hpp
+++ others/include/Position.hpp
@@ -8,9 +8,16 @@ struct Position
     Position() = default;
     
     Position operator==(Position& other) const;
+    void change_(float p_x, float p_y);
 };
 
 Position::Position operator==(Position& other) const
 {
     return((x==other.x) && (y==other.y));
+}
+
+void Position::change_pos(float p_x, float p_y)
+{
+    x += p_x;
+    y += p_y;
 }
